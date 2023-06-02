3e1e9636304388e5f59174c67d182cc9f250adaa
LUIZ CODE <beat.luiz23@hotmail.com>
2023-06-02 20:50:29 -0300

    atualização do script

--- src/js/arquivos_status.js
+++ src/js/arquivos_status.js
@@ -12,11 +12,11 @@ var card_ = document.querySelector('.pag-back-card');
 // variaveis dos nomes dos aquivos
 
 
-
 function card() {
     let button = document.getElementsByClassName('pag-projeto-numero-button');
     let nome_arquivo = document.querySelector('.js_nome_arquivo')
     let numeros = document.getElementsByClassName('pag-projeto-numero-button');
+    let pag_projeto = document.querySelector('.pag-projeto');
 
     // vai mostrar os botões das escolhas dos arquivos
     for (let index = 0; index < 10; index++) {
@@ -25,31 +25,14 @@ function card() {
 
         numeros[index].addEventListener("click", function (event) {
                   
-  if (event.target === numeros[0]) {
-      console.log("O botão foi pressionado!");
-  
+  if (event.target === numeros[index]) {
+      nome_arquivo.textContent="Card "+index+" "
+      pag_projeto.src = "../arquivos/HTML_JS_CSS/CARD 0" + index + "/"
+      // tem que seguir a mesma ordem de sequencia das pastas 
             }
-  if (event.target === numeros[0]) {
-      console.log("O botão foi pressionado!");
-  
-            }
-            
 
  });
     
     }
-    
-    
-  
-    
-    
-
-
-
-
-    
-
-
-
 }
 
