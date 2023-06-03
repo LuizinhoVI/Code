a352f7b67388df78cf49abc04d9857f37dc423b8
LUIZ CODE <beat.luiz23@hotmail.com>
2023-06-02 23:04:03 -0300

    atualização

--- src/js/arquivos_status.js
+++ src/js/arquivos_status.js
@@ -4,11 +4,6 @@ const total_de_arquivos = 46; //arquivos totais
 
 
 
-// variaveis dos arquivos 
-var card_ = document.querySelector('.pag-back-card');
-
-
-
 // variaveis dos nomes dos aquivos
 
 
@@ -18,6 +13,18 @@ function card() {
     let numeros = document.getElementsByClassName('pag-projeto-numero-button');
     let pag_projeto = document.querySelector('.pag-projeto');
 
+    pag_projeto.src = "../arquivos/HTML_JS_CSS/CARD 00/"
+    nome_arquivo.textContent = "CARD 0 ";
+    
+
+
+
+    //remover todos os botões numericos 
+    for (let remover = 0; remover < button.length; remover++) {
+    button[remover].style.display = "none";
+    }
+
+
     // vai mostrar os botões das escolhas dos arquivos
     for (let index = 0; index < 10; index++) {
         button[index].style.display = "block";
@@ -26,7 +33,7 @@ function card() {
         numeros[index].addEventListener("click", function (event) {
                   
   if (event.target === numeros[index]) {
-      nome_arquivo.textContent="Card "+index+" "
+      nome_arquivo.textContent="CARD "+index+" "
       pag_projeto.src = "../arquivos/HTML_JS_CSS/CARD 0" + index + "/"
       // tem que seguir a mesma ordem de sequencia das pastas 
             }
@@ -35,4 +42,145 @@ function card() {
     
     }
 }
+
+
+function button() {
+    let button = document.getElementsByClassName('pag-projeto-numero-button');
+    let nome_arquivo = document.querySelector('.js_nome_arquivo')
+    let numeros = document.getElementsByClassName('pag-projeto-numero-button');
+    let pag_projeto = document.querySelector('.pag-projeto');
+
+   
+   //nomes padrões 
+    pag_projeto.src = "../arquivos/HTML_JS_CSS/BUTTON 00/index.html"
+    nome_arquivo.textContent = "BUTTON 0"
+    
+    // vai mostrar os botões das escolhas dos arquivos
+
+    //remover todos os botões numericos 
+    for (let remover = 0; remover < button.length; remover++) {
+    button[remover].style.display = "none";
+    }
+
+    for (let index = 0; index < 4; index++) {
+        button[index].style.display = "block";
+
+            
+        
+
+        numeros[index].addEventListener("click", function (event) {
+            
+            if (event.target === numeros[index]) {
+                nome_arquivo.textContent = "BUTTON " + index + " ";
+                pag_projeto.src = "../arquivos/HTML_JS_CSS/BUTTON 0" + index + "/";
+                // tem que seguir a mesma ordem de sequencia das pastas 
+            }
+
+        });
+    }
+}
+    
+function switches() {
+    let button = document.getElementsByClassName('pag-projeto-numero-button');
+    let nome_arquivo = document.querySelector('.js_nome_arquivo')
+    let numeros = document.getElementsByClassName('pag-projeto-numero-button');
+    let pag_projeto = document.querySelector('.pag-projeto');
+
+   
+   //nomes padrões 
+    pag_projeto.src = "../arquivos/HTML_JS_CSS/Toggle switches 00"
+    nome_arquivo.textContent = "SWITCHES 0"
+    
+    // vai mostrar os botões das escolhas dos arquivos
+
+    //remover todos os botões numericos 
+    for (let remover = 0; remover < button.length; remover++) {
+    button[remover].style.display = "none";
+    }
+
+    for (let index = 0; index < 6; index++) {
+        button[index].style.display = "block";
+
+    
+        numeros[index].addEventListener("click", function (event) {
+            
+            if (event.target === numeros[index]) {
+                nome_arquivo.textContent = "SWITCHES " + index + "";
+                pag_projeto.src = "../arquivos/HTML_JS_CSS/Toggle switches 0" + index + "/"
+                // tem que seguir a mesma ordem de sequencia das pastas 
+            }
+
+        });
+    }
+}
+    
+function form() {
+    let button = document.getElementsByClassName('pag-projeto-numero-button');
+    let nome_arquivo = document.querySelector('.js_nome_arquivo')
+    let numeros = document.getElementsByClassName('pag-projeto-numero-button');
+    let pag_projeto = document.querySelector('.pag-projeto');
+
+   
+   //nomes padrões 
+    pag_projeto.src = "../arquivos/HTML_JS_CSS/FORMS 00"
+    nome_arquivo.textContent = "FORMS 0"
+    
+    // vai mostrar os botões das escolhas dos arquivos
+
+    //remover todos os botões numericos 
+    for (let remover = 0; remover < button.length; remover++) {
+    button[remover].style.display = "none";
+    }
+
+    for (let index = 0; index < 6; index++) {
+        button[index].style.display = "block";
+
+    
+        numeros[index].addEventListener("click", function (event) {
+            
+            if (event.target === numeros[index]) {
+                nome_arquivo.textContent = "FORMS  " + index + "";
+                pag_projeto.src = "../arquivos/HTML_JS_CSS/FORMS 0" + index + "/"
+                // tem que seguir a mesma ordem de sequencia das pastas 
+            }
+
+        });
+    }
+}
+    
+
+function loader() {
+    let button = document.getElementsByClassName('pag-projeto-numero-button');
+    let nome_arquivo = document.querySelector('.js_nome_arquivo')
+    let numeros = document.getElementsByClassName('pag-projeto-numero-button');
+    let pag_projeto = document.querySelector('.pag-projeto');
+
+   
+   //nomes padrões 
+    pag_projeto.src = "../arquivos/HTML_JS_CSS/LOADER 00"
+    nome_arquivo.textContent = "LOADER  0"
+    
+    // vai mostrar os botões das escolhas dos arquivos
+
+    //remover todos os botões numericos 
+    for (let remover = 0; remover < button.length; remover++) {
+    button[remover].style.display = "none";
+    }
+
+    for (let index = 0; index < 6; index++) {
+        button[index].style.display = "block";
+
+    
+        numeros[index].addEventListener("click", function (event) {
+            
+            if (event.target === numeros[index]) {
+                nome_arquivo.textContent = "LOADER   " + index + "";
+                pag_projeto.src = "../arquivos/HTML_JS_CSS/LOADER 0" + index + "/"
+                // tem que seguir a mesma ordem de sequencia das pastas 
+            }
+
+        });
+    }
+    }
+
 
