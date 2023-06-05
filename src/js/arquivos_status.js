0f6efcfe16a75ca909caa7096f45e2b5d6ab5e98
LUIZ CODE <beat.luiz23@hotmail.com>
2023-06-05 12:31:06 -0300

    atualização do script

--- src/js/arquivos_status.js
+++ src/js/arquivos_status.js
@@ -1,4 +1,6 @@
 // variaveis globais dos arquivo/*style*/`
+const ligar = true;
+const desligar = false;
 
 const total_de_arquivos = 46; //arquivos totais
 
@@ -10,33 +12,43 @@ const total_de_arquivos = 46; //arquivos totais
 function card() {
     let button = document.getElementsByClassName('pag-projeto-numero-button');
     let nome_arquivo = document.querySelector('.js_nome_arquivo')
-    let numeros = document.getElementsByClassName('pag-projeto-numero-button');
     let pag_projeto = document.querySelector('.pag-projeto');
-
+    let setas_numericas = document.querySelector(".pag-projeto-numero-setas");
+ 
+  
+   
     pag_projeto.src = "../arquivos/HTML_JS_CSS/CARD 00/"
     nome_arquivo.textContent = "CARD 0 ";
-    
 
+   
+    numeros_buttons(ligar, desligar,0);
 
-
-    //remover todos os botões numericos 
-    for (let remover = 0; remover < button.length; remover++) {
-    button[remover].style.display = "none";
-    }
-
+   
 
     // vai mostrar os botões das escolhas dos arquivos
     for (let index = 0; index < 10; index++) {
         button[index].style.display = "block";
 
 
-        numeros[index].addEventListener("click", function (event) {
+        
+      if (index <=7) {
+      setas_numericas.style.opacity ="1%"
+      } else {
+          setas_numericas.style.opacity ="100%"
+      }
+
+        
+        button[index].addEventListener("click", function (event) {
                   
-  if (event.target === numeros[index]) {
+  if (event.target === button[index]) {
       nome_arquivo.textContent="CARD "+index+" "
       pag_projeto.src = "../arquivos/HTML_JS_CSS/CARD 0" + index + "/"
-      // tem que seguir a mesma ordem de sequencia das pastas 
-            }
+      // tem que seguir a mesma ordem de sequencia das pastas
+    
+    
+    numeros_buttons(ligar, desligar,index);
+  
+  } 
 
  });
     
@@ -49,23 +61,25 @@ function button() {
     let nome_arquivo = document.querySelector('.js_nome_arquivo')
     let numeros = document.getElementsByClassName('pag-projeto-numero-button');
     let pag_projeto = document.querySelector('.pag-projeto');
-
+    let setas_numericas = document.querySelector(".pag-projeto-numero-setas");
    
    //nomes padrões 
     pag_projeto.src = "../arquivos/HTML_JS_CSS/BUTTON 00/index.html"
     nome_arquivo.textContent = "BUTTON 0"
+
     
-    // vai mostrar os botões das escolhas dos arquivos
 
-    //remover todos os botões numericos 
-    for (let remover = 0; remover < button.length; remover++) {
-    button[remover].style.display = "none";
-    }
-
+    numeros_buttons(ligar, ligar,0);
+    
+    
+    
     for (let index = 0; index < 4; index++) {
         button[index].style.display = "block";
 
-            
+              if (index <=7) {
+      setas_numericas.style.opacity ="1%"
+      }
+        
         
 
         numeros[index].addEventListener("click", function (event) {
@@ -73,7 +87,14 @@ function button() {
             if (event.target === numeros[index]) {
                 nome_arquivo.textContent = "BUTTON " + index + " ";
                 pag_projeto.src = "../arquivos/HTML_JS_CSS/BUTTON 0" + index + "/";
-                // tem que seguir a mesma ordem de sequencia das pastas 
+                // tem que seguir a mesma ordem de sequencia das pastas
+
+
+    
+                numeros_buttons(ligar, desligar,index);
+                
+
+  
             }
 
         });
@@ -85,29 +106,35 @@ function switches() {
     let nome_arquivo = document.querySelector('.js_nome_arquivo')
     let numeros = document.getElementsByClassName('pag-projeto-numero-button');
     let pag_projeto = document.querySelector('.pag-projeto');
-
+ let setas_numericas = document.querySelector(".pag-projeto-numero-setas");
    
    //nomes padrões 
     pag_projeto.src = "../arquivos/HTML_JS_CSS/Toggle switches 00"
     nome_arquivo.textContent = "SWITCHES 0"
-    
-    // vai mostrar os botões das escolhas dos arquivos
+  
 
-    //remover todos os botões numericos 
-    for (let remover = 0; remover < button.length; remover++) {
-    button[remover].style.display = "none";
-    }
+    
+    numeros_buttons(ligar, ligar,0);
+    
 
     for (let index = 0; index < 6; index++) {
         button[index].style.display = "block";
 
+          if (index <=7) {
+    setas_numericas.style.opacity ="1%"
+      }
+        
     
         numeros[index].addEventListener("click", function (event) {
             
             if (event.target === numeros[index]) {
                 nome_arquivo.textContent = "SWITCHES " + index + "";
                 pag_projeto.src = "../arquivos/HTML_JS_CSS/Toggle switches 0" + index + "/"
-                // tem que seguir a mesma ordem de sequencia das pastas 
+                // tem que seguir a mesma ordem de sequencia das pastas
+
+                
+    numeros_buttons(ligar, desligar,index);
+    
             }
 
         });
@@ -119,21 +146,24 @@ function form() {
     let nome_arquivo = document.querySelector('.js_nome_arquivo')
     let numeros = document.getElementsByClassName('pag-projeto-numero-button');
     let pag_projeto = document.querySelector('.pag-projeto');
-
+ let setas_numericas = document.querySelector(".pag-projeto-numero-setas");
    
    //nomes padrões 
     pag_projeto.src = "../arquivos/HTML_JS_CSS/FORMS 00"
     nome_arquivo.textContent = "FORMS 0"
     
-    // vai mostrar os botões das escolhas dos arquivos
 
-    //remover todos os botões numericos 
-    for (let remover = 0; remover < button.length; remover++) {
-    button[remover].style.display = "none";
-    }
-
+    numeros_buttons(ligar, ligar,0);
+    
     for (let index = 0; index < 6; index++) {
         button[index].style.display = "block";
+
+  if (index <=7) {
+       setas_numericas.style.opacity ="1%"
+      }
+        
+
+
 
     
         numeros[index].addEventListener("click", function (event) {
@@ -142,7 +172,9 @@ function form() {
                 nome_arquivo.textContent = "FORMS  " + index + "";
                 pag_projeto.src = "../arquivos/HTML_JS_CSS/FORMS 0" + index + "/"
                 // tem que seguir a mesma ordem de sequencia das pastas 
-            }
+            
+            numeros_buttons(ligar, desligar,index);
+    }
 
         });
     }
@@ -151,36 +183,67 @@ function form() {
 
 function loader() {
     let button = document.getElementsByClassName('pag-projeto-numero-button');
-    let nome_arquivo = document.querySelector('.js_nome_arquivo')
+    let nome_arquivo = document.querySelector('.js_nome_arquivo');
     let numeros = document.getElementsByClassName('pag-projeto-numero-button');
     let pag_projeto = document.querySelector('.pag-projeto');
-
+    let setas_numericas = document.querySelector(".pag-projeto-numero-setas");
+   
    
    //nomes padrões 
     pag_projeto.src = "../arquivos/HTML_JS_CSS/LOADER 00"
     nome_arquivo.textContent = "LOADER  0"
-    
-    // vai mostrar os botões das escolhas dos arquivos
+ 
 
-    //remover todos os botões numericos 
-    for (let remover = 0; remover < button.length; remover++) {
-    button[remover].style.display = "none";
-    }
+    // deixa todos os botões invisivel e mostrar só um 
+    numeros_buttons(ligar, ligar,0);
+    //************************************ */
 
+    // mostra todos os botões numericos
     for (let index = 0; index < 6; index++) {
         button[index].style.display = "block";
+
+      if (index <=7) {
+        setas_numericas.style.opacity ="1%"
+      }
+        
 
     
         numeros[index].addEventListener("click", function (event) {
             
             if (event.target === numeros[index]) {
                 nome_arquivo.textContent = "LOADER   " + index + "";
-                pag_projeto.src = "../arquivos/HTML_JS_CSS/LOADER 0" + index + "/"
+                pag_projeto.src = "../arquivos/HTML_JS_CSS/LOADER 0" + index + "/";
                 // tem que seguir a mesma ordem de sequencia das pastas 
-            }
+           
+    numeros_buttons(ligar, desligar,index);
+     }
 
         });
     }
     }
 
 
+// removedor de numeros e arquivos e buttons 
+function numeros_buttons(numero,buttons,escolha) {
+    let button = document.getElementsByClassName('pag-projeto-numero-button');
+    
+
+    if (buttons) {
+             //remover todos os botões numericos 
+    for (let remover = 0; remover < button.length; remover++) {
+    button[remover].style.display = "none";
+        }
+        
+    }
+
+ if (numero) {
+      //remover todos os botões numericos pre-selecionados azuis 
+    for (let remover_class = 0; remover_class < button.length; remover_class++) {
+    button[remover_class].classList.remove('pag-projeto-numero-button-on')
+      }
+     
+    }
+    
+    button[escolha].classList.add('pag-projeto-numero-button-on');
+  
+}
